d6174c0c7d1fbe1be7f3dac9644c4c6fed2670fa
YushunChen <56765896+YushunChen@users.noreply.github.com>
2021-09-20 16:41:16 -0500

    Lab 2 Calculator - Completed overall functionality

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,9 +2,42 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void calculate(View view) {
+        EditText firstNumberField = (EditText) findViewById(R.id.firstNumberField);
+        EditText secondNumberField = (EditText) findViewById(R.id.secondNumberField);
+        int firstNumber = Integer.parseInt(firstNumberField.getText().toString());
+        int secondNumber = Integer.parseInt(secondNumberField.getText().toString());
+
+        Button b = (Button) view;
+        String operation = b.getText().toString();
+        int result = 0;
+
+        if (operation.equals("+")) {
+            result = firstNumber + secondNumber;
+        } else if (operation.equals("-")) {
+            result = firstNumber - secondNumber;
+        } else if (operation.equals("×")) {
+            result = firstNumber * secondNumber;
+        } else {
+            result = firstNumber / secondNumber;
+        }
+
+        goToActivity2(Integer.toString(result));
+    }
+
+    public void goToActivity2(String s) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("result", s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
